efe72a66e2461e45868a71862fb6a37b1d34cd32
Bibika Gurbanguliyeva <ogulbibigurbangulyeva@gmail.com>
2025-10-06 14:55:06 +0500

    added images 6.10-2

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,13 @@
+// vite.config.ts
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  base: './', // ключевая строка: относительные пути к assets
+  plugins: [vue(), vueDevTools()],
   resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
+    alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) },
   },
 })
